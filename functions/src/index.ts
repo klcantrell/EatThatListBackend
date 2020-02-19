4518a873dc595bf202dd0e8cb47385a81e80cd5e
klcantrell <cantrellkalalau@gmail.com>
2020-02-18 21:11:34 -0500

    Add trigger for adding owner list access

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,11 @@ admin.initializeApp(functions.config().firebase);
 
 const GRAPHQL_ENDPOINT = 'https://eat-that-list.herokuapp.com/v1/graphql';
 
+interface GraphQLRequest {
+  query: string;
+  variables: {};
+}
+
 export const processSignUp = functions.auth.user().onCreate(async user => {
   const { uid: userId, email } = user;
   const customClaims = {
@@ -30,12 +35,8 @@ export const processSignUp = functions.auth.user().onCreate(async user => {
       email,
     },
   };
-  await axios.post(GRAPHQL_ENDPOINT, JSON.stringify(graphqlInsertUserRequest), {
-    headers: {
-      'content-type': 'application/json',
-      'x-hasura-admin-secret': `${functions.config().hasura.admin.secret}`,
-    },
-  });
+
+  await sendGraphqlRequest(graphqlInsertUserRequest);
 
   return admin
     .auth()
@@ -46,3 +47,58 @@ export const processSignUp = functions.auth.user().onCreate(async user => {
     })
     .catch(console.log);
 });
+
+export const addOwnerListAccess = functions.https.onRequest(
+  async (req, res) => {
+    const reqSecret = req.get('x-hasura-admin-secret');
+    if (reqSecret !== functions.config().hasura.admin.secret) {
+      console.log(
+        'Unauthenticated call to this endpoint',
+        req.body,
+        req.headers
+      );
+      res.end();
+    }
+
+    const listId = req.body.event.data.new.id;
+    const userId = req.body.event.session_variables['x-hasura-user-id'];
+
+    const addOwnerQuery = `
+      mutation($listId: Int!, $userId: String!) {
+        insert_ListAccess(objects: [{ list_id: $listId, user_id: $userId }]) {
+          affected_rows
+        }
+      }
+    `;
+    const graphqlAddOwnerRequest = {
+      query: addOwnerQuery,
+      variables: {
+        listId,
+        userId,
+      },
+    };
+
+    let retries = 0;
+    let result = await sendGraphqlRequest(graphqlAddOwnerRequest);
+
+    while (result.status !== 200) {
+      retries = retries + 1;
+      console.log(
+        `Problem sending request to Hasura.  Proceeding with retry ${retries}`,
+        result.data
+      );
+      result = await sendGraphqlRequest(graphqlAddOwnerRequest);
+    }
+
+    res.end();
+  }
+);
+
+const sendGraphqlRequest = (request: GraphQLRequest) => {
+  return axios.post(GRAPHQL_ENDPOINT, JSON.stringify(request), {
+    headers: {
+      'content-type': 'application/json',
+      'x-hasura-admin-secret': `${functions.config().hasura.admin.secret}`,
+    },
+  });
+};
